3b46adccef041d19410c312e41b1ded4c027b160
kldoc <fred.berbigier@gmail.com>
2019-11-02 14:42:05 +0800

    [2/2]Settings: Pulse and Ambient notification bars
    Add Pulse and Ambient Display notification bars
    
    Change-Id: I96755ced4d4ff676cfe6b59adfcdbe19b36a3a1d
    Signed-off-by: drpradsms <dr.pradsms@gmail.com>

--- src/com/nitrogen/settings/fragments/NotificationSettings.java
+++ src/com/nitrogen/settings/fragments/NotificationSettings.java
@@ -12,11 +12,15 @@ import androidx.preference.PreferenceScreen;
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.nitrogen.settings.preferences.Utils;
+import com.nitrogen.settings.preferences.SystemSettingSwitchPreference;
 
 public class NotificationSettings extends SettingsPreferenceFragment {
 
     private Preference mChargingLeds;
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String PULSE_AMBIANT_LIGHT_PREF = "pulse_ambient_light";
+
+    private SystemSettingSwitchPreference mPulseEdgeLights;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -25,6 +29,11 @@ public class NotificationSettings extends SettingsPreferenceFragment {
         addPreferencesFromResource(R.xml.nitrogen_settings_notifications);
 
         PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mPulseEdgeLights = (SystemSettingSwitchPreference) findPreference(PULSE_AMBIANT_LIGHT_PREF);
+        boolean mPulseNotificationEnabled = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.DOZE_ENABLED, 0) != 0;
+        mPulseEdgeLights.setEnabled(mPulseNotificationEnabled);
 
         mChargingLeds = (Preference) findPreference("charging_light");
         if (mChargingLeds != null
