66a2d41abffdb521d6bb7d468cf7c31b3e4e7793
drpradsms <dr.pradsms@gmail.com>
2020-01-18 00:05:55 -0800

    Revert "Powermenu Torch option [2/2]"
    This reverts commit ced1718506cbae9e8b170cbfad62b13bcbf856a0.
    
    Signed-off-by: drpradsms <dr.pradsms@gmail.com>

--- src/com/nitrogen/settings/fragments/PowerMenuSettings.java
+++ src/com/nitrogen/settings/fragments/PowerMenuSettings.java
@@ -40,17 +40,14 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.nitrogen.settings.preferences.Utils;
 
 public class PowerMenuSettings extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_POWERMENU_LOGOUT = "powermenu_logout";
-    private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
     private static final String KEY_POWERMENU_USERS = "powermenu_users";
 
     private SwitchPreference mPowermenuLogout;
-    private SwitchPreference mPowermenuTorch;
     private SwitchPreference mPowermenuUsers;
 
     private UserManager mUserManager;
@@ -64,14 +61,6 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
-        mPowermenuTorch = (SwitchPreference) findPreference(KEY_POWERMENU_TORCH);
-        mPowermenuTorch.setOnPreferenceChangeListener(this);
-        if (!Utils.deviceSupportsFlashLight(getActivity())) {
-            prefScreen.removePreference(mPowermenuTorch);
-        } else {
-        mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
-                Settings.System.POWERMENU_TORCH, 0) == 1));
-        }
 
         mPowermenuLogout = (SwitchPreference) findPreference(KEY_POWERMENU_LOGOUT);
         mPowermenuLogout.setOnPreferenceChangeListener(this);
@@ -94,11 +83,6 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWERMENU_LOGOUT, value ? 1 : 0);
-            return true;
-        } else if (preference == mPowermenuTorch) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.POWERMENU_TORCH, value ? 1 : 0);
             return true;
         } else if (preference == mPowermenuUsers) {
             boolean value = (Boolean) newValue;
